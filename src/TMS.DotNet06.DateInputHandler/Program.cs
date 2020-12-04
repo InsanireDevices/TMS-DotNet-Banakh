f9999c50a5a67e0d597527dee20d16db33c48017
Kirill Banakh <66416263+InsanireDevices@users.noreply.github.com>
2020-12-04 10:13:01 +0300

    init: Add project.

---
+++ src/TMS.DotNet06.DateInputHandler/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TMS.DotNet06.DateInputHandler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
