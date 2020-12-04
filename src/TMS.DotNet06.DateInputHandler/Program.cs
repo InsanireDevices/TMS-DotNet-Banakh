8a2bc98058de897988c0a3bcaa5229109dbf68ec
Kirill Banakh <66416263+InsanireDevices@users.noreply.github.com>
2020-12-04 11:03:56 +0300

    feat: Task is done.

--- src/TMS.DotNet06.DateInputHandler/Program.cs
+++ src/TMS.DotNet06.DateInputHandler/Program.cs
@@ -6,7 +6,37 @@ namespace TMS.DotNet06.DateInputHandler
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            DateTime DateToCheck;
+            Boolean DateIsCorrect = false;
+            
+            while(!DateIsCorrect)
+            {
+                try
+                {
+                    Console.ForegroundColor = ConsoleColor.Cyan;
+                    Console.Write("Please enter date : ");
+                    Console.ResetColor();
+
+                    DateToCheck = DateTime.Parse(Console.ReadLine());         
+                    
+                    Console.Write("Day is : ");
+                    
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.Write(DateToCheck.DayOfWeek + "\n");
+                    Console.ResetColor();
+                    
+                    DateIsCorrect = true;
+                }
+                catch (Exception)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Error: Incorrect date! Please, enter date in yyyy-mm-dd format.");
+                    Console.ResetColor();
+                }
+            }
+
+            Console.WriteLine("Press any key to close app.");
+            Console.ReadKey();
         }
     }
 }
