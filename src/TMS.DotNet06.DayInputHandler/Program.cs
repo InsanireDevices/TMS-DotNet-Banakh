e98a99f2b09eb5c8db5645eb9388b002d6087027
Kirill Banakh <66416263+InsanireDevices@users.noreply.github.com>
2020-12-07 16:00:27 +0300

    feat: Add day output handler.

--- src/TMS.DotNet06.DayInputHandler/Program.cs
+++ src/TMS.DotNet06.DayInputHandler/Program.cs
@@ -10,21 +10,8 @@ namespace TMS.DotNet06.DayInputHandler
 
             DayInputHandler();
 
+            Console.WriteLine("Press any key to close app.");
             Console.ReadKey();
-
-            //Console.WriteLine("--------------------------------");
-
-            //Console.WriteLine("START day : " + DayToStart.ToString("dd/MM/yyyy", CultureInfo.InvariantCulture));
-            //Console.WriteLine("END day : " + DayToEnd.ToString("dd/MM/yyyy", CultureInfo.InvariantCulture));
-
-            //Console.WriteLine("--------------------------------");
-            
-            //DaySearch(DayToStart, DayToEnd);
-
-        }
-
-        static void DateOutputHandler(DateTime dayToStart, DateTime dayToEnd)
-        {
 
         }
 
@@ -41,8 +28,7 @@ namespace TMS.DotNet06.DayInputHandler
 
             if (DayToStart < DayToEnd)
             {
-                Console.WriteLine("Correct Input");
-                DateOutputHandler(DayToStart, DayToEnd);
+                DayOutputHandler(DayToStart, DayToEnd);
             }
 
             else
@@ -55,25 +41,70 @@ namespace TMS.DotNet06.DayInputHandler
             }
                 
         }
+        static void DayOutputHandler(DateTime dayToStart, DateTime dayToEnd)
+        {
+            DayOfWeek dayOfWeek;
+
+            Console.WriteLine("--------------------------------");
+            Console.WriteLine("START day : " + dayToStart.ToString("dd/MM/yyyy", CultureInfo.InvariantCulture));
+            Console.WriteLine("END day : " + dayToEnd.ToString("dd/MM/yyyy", CultureInfo.InvariantCulture));
+            Console.WriteLine("--------------------------------");
+
+            Console.Write("Enter DAY OF WEEK to search : ");
+
+            dayOfWeek = DayOfWeekInputCheck(Console.ReadLine(), "Enter DAY OF WEEK to search: ");
+
+            Console.WriteLine("DAY OF WEEK entered : " + dayOfWeek);
+            Console.WriteLine("--------------------------------");
+
+            while (dayToEnd >= dayToStart)
+            {
+                if (dayToStart.DayOfWeek == dayOfWeek)
+                {
+                    Console.WriteLine(dayToStart.ToString("dd/MM/yyyy - dddd", CultureInfo.InvariantCulture));
+                }
+                dayToStart = dayToStart.AddDays(1);
+            }
+
+        }
+
+        static DayOfWeek DayOfWeekInputCheck(string dayOfWeekInput, string request)
+        {
+            try
+            {
+                DayOfWeek dayOfWeek  = Enum.Parse<DayOfWeek>(dayOfWeekInput);
+                return dayOfWeek;
+            }
+            catch (Exception)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Error : Invalid Input!");
+                Console.ResetColor();
+
+                Console.Write(request);
+
+                return DayOfWeekInputCheck(Console.ReadLine(), request);
+            }
+        }
 
         static DateTime DayInputCheck(string dayInput, string request)
         {
             DateTime dayOutput;
 
-                try
-                {
-                    return dayOutput = DateTime.Parse(dayInput);
-                }
-                catch (Exception)
-                {
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    Console.WriteLine("Error : Input is Incorrect!");
-                    Console.ResetColor();
+            try
+            {
+                return dayOutput = DateTime.Parse(dayInput);
+            }
+            catch (Exception)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Error : Input is Incorrect!");
+                Console.ResetColor();
 
-                    Console.Write(request);
+                Console.Write(request);
 
-                    return DayInputCheck(Console.ReadLine(),request);
-                }
+                return DayInputCheck(Console.ReadLine(),request);
+            }
                 
         }
     }
