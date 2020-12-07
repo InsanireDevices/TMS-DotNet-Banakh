f84c174a1eaeea4eedb1cf58c64fc6652ae49ef8
Kirill Banakh <66416263+InsanireDevices@users.noreply.github.com>
2020-12-07 12:03:27 +0300

    feat: Add method to check input day.

--- src/TMS.DotNet06.DayInputHandler/Program.cs
+++ src/TMS.DotNet06.DayInputHandler/Program.cs
@@ -6,7 +6,38 @@ namespace TMS.DotNet06.DayInputHandler
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            DateTime DayToStart;
+            DateTime DayToEnd;
+
+            Console.Write("Enter START day : ");
+            DayToStart = DayInputCheck(Console.ReadLine(), "Enter START day : ");
+
+            Console.WriteLine("Enter END day : ");
+            DayToEnd = DayInputCheck(Console.ReadLine(), "Enter END day:");
+        }
+
+
+
+        static DateTime DayInputCheck(string dayInput, string request)
+        {
+            DateTime dayOutput;
+
+                try
+                {
+                    return dayOutput = DateTime.Parse(dayInput);
+                }
+                catch (Exception)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Error : Input is Incorrect!");
+                    Console.ResetColor();
+
+                    Console.Write(request);
+
+                    return DayInputCheck(Console.ReadLine(),request);
+                }
+                
         }
     }
-}
+
+ }
