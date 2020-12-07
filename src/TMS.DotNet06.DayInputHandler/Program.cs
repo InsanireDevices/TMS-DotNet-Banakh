8be6f7ae2c59b94b38373c3243f9036a74fe9eee
Kirill Banakh <66416263+InsanireDevices@users.noreply.github.com>
2020-12-07 14:30:39 +0300

    feat: Add input day handler.

--- src/TMS.DotNet06.DayInputHandler/Program.cs
+++ src/TMS.DotNet06.DayInputHandler/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace TMS.DotNet06.DayInputHandler
 {
@@ -6,17 +7,54 @@ namespace TMS.DotNet06.DayInputHandler
     {
         static void Main(string[] args)
         {
+
+            DayInputHandler();
+
+            Console.ReadKey();
+
+            //Console.WriteLine("--------------------------------");
+
+            //Console.WriteLine("START day : " + DayToStart.ToString("dd/MM/yyyy", CultureInfo.InvariantCulture));
+            //Console.WriteLine("END day : " + DayToEnd.ToString("dd/MM/yyyy", CultureInfo.InvariantCulture));
+
+            //Console.WriteLine("--------------------------------");
+            
+            //DaySearch(DayToStart, DayToEnd);
+
+        }
+
+        static void DateOutputHandler(DateTime dayToStart, DateTime dayToEnd)
+        {
+
+        }
+
+        static void DayInputHandler()
+        {
             DateTime DayToStart;
             DateTime DayToEnd;
 
             Console.Write("Enter START day : ");
             DayToStart = DayInputCheck(Console.ReadLine(), "Enter START day : ");
 
-            Console.WriteLine("Enter END day : ");
-            DayToEnd = DayInputCheck(Console.ReadLine(), "Enter END day:");
+            Console.Write("Enter END day : ");
+            DayToEnd = DayInputCheck(Console.ReadLine(), "Enter END day : ");
+
+            if (DayToStart < DayToEnd)
+            {
+                Console.WriteLine("Correct Input");
+                DateOutputHandler(DayToStart, DayToEnd);
+            }
+
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Error : END day is less than START day!");
+                Console.ResetColor();
+
+                DayInputHandler();
+            }
+                
         }
-
-
 
         static DateTime DayInputCheck(string dayInput, string request)
         {
@@ -39,5 +77,4 @@ namespace TMS.DotNet06.DayInputHandler
                 
         }
     }
-
  }
