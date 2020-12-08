d42daa1360bbefcdbe626bfeaedd0ebd2606260b
Kirill Banakh <66416263+InsanireDevices@users.noreply.github.com>
2020-12-08 10:20:56 +0300

    init: Add project and TaskList class.

---
+++ src/TMS.DotNet06.TaskList/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace TMS.DotNet06.TaskList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine();
+        }
+    }
+}
---
+++ src/TMS.DotNet06.TaskList/TaskList.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TMS.DotNet06.TaskList
+{
+    class TaskList
+    {
+    }
+}
