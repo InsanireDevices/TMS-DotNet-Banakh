aa16d9adcf48ed1ddd8af286ced87f8a9ad17fdf
Kirill Banakh <66416263+InsanireDevices@users.noreply.github.com>
2020-12-09 17:19:23 +0300

    feat: Add method to Edit Tasks.

--- src/TMS.DotNet06.TaskList/TaskList.cs
+++ src/TMS.DotNet06.TaskList/TaskList.cs
@@ -73,7 +73,6 @@ namespace TMS.DotNet06.TaskList
             string description;
             DateTime startDate;
             DateTime endDate;
-
             Console.Clear();
 
             ShowHeadLine();
@@ -120,6 +119,13 @@ namespace TMS.DotNet06.TaskList
 
         public void Edit()
         {
+            string description;
+            DateTime startDate;
+            DateTime endDate;
+            string status;
+
+            int task;
+
             Console.Clear();
 
             ShowHeadLine();
@@ -127,6 +133,107 @@ namespace TMS.DotNet06.TaskList
             Console.WriteLine("------------------");
             Console.WriteLine("|   Edit Task    |");
             Console.WriteLine("------------------\n");
+
+            if (this.taskCards.Count == 0)
+            {
+                Console.Clear();
+                ShowHeadLine();
+                Console.WriteLine("ERROR : Your Task List is Empty!\n");
+                ShowMenu();
+                MenuInputHanler();
+            }
+
+            else
+            {
+                for (int i = 0; i < this.taskCards.Count; i++)
+                {
+                    Console.WriteLine($"------Task {i + 1}------");
+                    Console.WriteLine($"ID : {taskCards[i].ID}");
+                    Console.WriteLine($"Description : {taskCards[i].Description}");
+                    Console.WriteLine($"Timing :  {taskCards[i].StartDate.ToString("dd.MM.yyyy")} -> {taskCards[i].EndDate.ToString("dd.MM.yyyy")}");
+                    Console.WriteLine($"Status : {taskCards[i].Status}");
+                    Console.WriteLine("------------------\n");
+                }
+
+                
+            
+            taskEditInput:
+                Console.Write("Choose Task to Edit : ");
+
+                try
+                {
+                    task = Convert.ToInt32(Convert.ToUInt32(Console.ReadLine()) - 1);
+                }
+                catch (Exception)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("ERROR : Must be number greater than zero!");
+                    Console.ResetColor();
+                    goto taskEditInput;
+                }
+
+                Console.Clear();
+                ShowHeadLine();
+
+                Console.WriteLine("------------------");
+                Console.WriteLine("|   Edit Task    |");
+                Console.WriteLine("------------------\n");
+
+                Console.WriteLine($"------Task {task + 1}------");
+                Console.WriteLine($"ID : {taskCards[task].ID}");
+                Console.WriteLine($"Description : {taskCards[task].Description}");
+                Console.WriteLine($"Timing :  {taskCards[task].StartDate.ToString("dd.MM.yyyy")} -> {taskCards[task].EndDate.ToString("dd.MM.yyyy")}");
+                Console.WriteLine($"Status : {taskCards[task].Status}");
+                Console.WriteLine("------------------\n");
+
+                Console.Write("Enter Description : ");
+                description = Console.ReadLine();
+
+            dateInput:
+
+                Console.Write("Ented Start Date : ");
+                startDate = DateInputCheck(Console.ReadLine(), "Ented Start Date : ");
+
+                Console.Write("Enter End Date : ");
+                endDate = DateInputCheck(Console.ReadLine(), "Enter End Date : ");
+
+                if (startDate > endDate)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("ERROR : END day is less than START day!");
+                    Console.ResetColor();
+
+                    goto dateInput;
+                }
+
+            statusInput:
+                Console.Write("Enter ToDo\\InProgress\\Done Status : ");
+                try
+                {
+                    status = Enum.Parse<TaskCard.StatusList>(Console.ReadLine()).ToString();
+                }
+                catch (Exception)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("ERROR : Status can take only next values : ToDo, InProgress, Done");
+                    Console.ResetColor();
+                    goto statusInput;
+                }
+
+                
+
+                taskCards[task].Description = description;
+                taskCards[task].StartDate = startDate;
+                taskCards[task].EndDate = endDate;
+                taskCards[task].Status = status;
+
+                Console.Clear();
+                ShowHeadLine();
+                Console.WriteLine("Task succesfully edited!\n");
+                ShowMenu();
+                MenuInputHanler();
+            }
+
             ShowMenu();
             MenuInputHanler();
         }
@@ -164,9 +271,9 @@ namespace TMS.DotNet06.TaskList
                     Console.WriteLine("------------------\n");
                 }
 
-                Console.Write("Choose Task to Delete : ");
-
             taskInput:
+                Console.Write("Choose Task to Delete : ");
+                
                 try
                 {
                     taskCards.Remove(taskCards[Convert.ToInt32(Console.ReadLine()) - 1]);
