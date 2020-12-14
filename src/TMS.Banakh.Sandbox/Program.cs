5f7fe97283c43ec44b78a8ef6c5ce0771d8fa518
Kirill Banakh <66416263+InsanireDevices@users.noreply.github.com>
2020-12-14 19:10:05 +0300

    init: Add Sandbox Project

---
+++ src/TMS.Banakh.Sandbox/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TMS.Banakh.Sandbox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
