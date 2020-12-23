32f48c475c5bfab1108fcdc1e1d52c62a16231bd
Kirill Banakh <66416263+InsanireDevices@users.noreply.github.com>
2020-12-23 10:37:52 +0300

    init: Add project.

---
+++ src/TMS.DotNet06.ZooManager/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace TMS.DotNet06.ZooManager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
