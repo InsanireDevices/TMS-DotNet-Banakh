f227ce539c18b3db23cdc3a95634a2b47827d69a
Kirill Banakh <66416263+InsanireDevices@users.noreply.github.com>
2020-12-24 10:30:36 +0300

    feat: Add AnimalBase class

---
+++ src/TMS.DotNet06.ZooManager/Models/AnimalBase.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TMS.DotNet06.ZooManager.Models
+{
+    class AnimalBase
+    {
+        public string name { get; set; }
+        public string sex { get; set; }
+        public int age { get; set; }
+    }
+}
