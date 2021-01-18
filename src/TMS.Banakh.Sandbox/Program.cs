413f46e4e0c3d7196c1495e61ddd569ebc5ab720
Kirill Banakh <66416263+InsanireDevices@users.noreply.github.com>
2021-01-18 19:03:06 +0300

    clear

--- src/TMS.Banakh.Sandbox/Program.cs
+++ src/TMS.Banakh.Sandbox/Program.cs
@@ -7,29 +7,6 @@ namespace TMS.Banakh.Sandbox
     {
         static void Main(string[] args)
         {
-            var delegateTest = new Delegates("kokoko");
-            delegateTest.message1 = (int num, string str) =>
-            {
-                Console.WriteLine("hohoh");
-                return true;
-            };
-            //delegateTest.message1 = delegateTest.ShowMessage;
-            delegateTest.message1(1, "lolool");
-
-            Action<int> action = (int num) =>
-            {
-                Console.WriteLine(num);
-            };
-
-            Predicate<int> predicate = (num) => { return num > 0; };
-
-            Func<int, string> func = (num) => { return num.ToString(); };
-
-            Console.WriteLine(func(12));
-
-            Console.WriteLine(predicate(-1));
-
-            action(10);
 
             Console.ReadKey();
         }
