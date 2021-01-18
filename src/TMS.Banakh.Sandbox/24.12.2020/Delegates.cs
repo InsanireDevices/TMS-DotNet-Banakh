564c6271415f429df17ebb99aa7e71273d664aac
Kirill Banakh <66416263+InsanireDevices@users.noreply.github.com>
2021-01-18 19:01:16 +0300

    feat: delegate

---
+++ src/TMS.Banakh.Sandbox/24.12.2020/Delegates.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TMS.Banakh.Sandbox._24._12._2020
+{
+    public class Delegates
+    {
+        private object _someobject;
+        public Delegates(string somestring)
+        {
+            _someobject = somestring;
+        }
+
+        //public delegate void Message();
+        //public Message message1;
+        //public Action message1;
+
+        public Func<int, string, bool> message1;
+
+        //public Predicate<int> message1;
+
+        //public bool ShowMessage(int number)
+        //{
+
+        //    Console.WriteLine(_someobject);
+        //    return true;
+        //}
+
+        public void ShowMessage()
+        {
+            Console.WriteLine(_someobject);
+        }
+    }
+}
--- src/TMS.Banakh.Sandbox/Program.cs
+++ src/TMS.Banakh.Sandbox/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using TMS.Banakh.Sandbox._24._12._2020;
 
 namespace TMS.Banakh.Sandbox
 {
@@ -6,15 +7,33 @@ namespace TMS.Banakh.Sandbox
     {
         static void Main(string[] args)
         {
-            var penOne = new Pen();
-            var penTwo = new Pen(123);
+            var delegateTest = new Delegates("kokoko");
+            delegateTest.message1 = (int num, string str) =>
+            {
+                Console.WriteLine("hohoh");
+                return true;
+            };
+            //delegateTest.message1 = delegateTest.ShowMessage;
+            delegateTest.message1(1, "lolool");
 
-            Console.WriteLine(penOne);
-            Console.WriteLine(penOne.Color);
-            Console.WriteLine(penTwo);
-            Console.WriteLine(penTwo.Color);
+            Action<int> action = (int num) =>
+            {
+                Console.WriteLine(num);
+            };
+
+            Predicate<int> predicate = (num) => { return num > 0; };
+
+            Func<int, string> func = (num) => { return num.ToString(); };
+
+            Console.WriteLine(func(12));
+
+            Console.WriteLine(predicate(-1));
+
+            action(10);
 
             Console.ReadKey();
         }
+
+
     }
 }
