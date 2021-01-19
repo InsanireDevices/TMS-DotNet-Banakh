a4b65fc5d07869dcb7d2b117ae8da03178741214
Kirill Banakh <66416263+InsanireDevices@users.noreply.github.com>
2021-01-19 12:47:02 +0300

    init: Add Fitness Tracker project.

---
+++ src/TMS.DotNet06.FitnessTracker/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace TMS.DotNet06.FitnessTracker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Fitness Tracker!");
+            Console.ReadKey();
+        }
+    }
+}
