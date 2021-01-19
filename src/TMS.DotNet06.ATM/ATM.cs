a9e3c2913693dabe50f3d52140b14b6a37b42c7e
Kirill Banakh <66416263+InsanireDevices@users.noreply.github.com>
2021-01-19 10:52:18 +0300

    feat: Add ATM menu and BankAccount class

---
+++ src/TMS.DotNet06.ATM/ATM.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TMS.DotNet06.ATM
+{
+    class ATM
+    {
+        public ATM()
+        {
+        }
+        
+        void ShowMenu()
+        {
+            Console.WriteLine("-------------MENU------------");
+            Console.WriteLine("Top Up Account         -> [T]");
+            Console.WriteLine("Withdraw From Account  -> [W]");
+            Console.WriteLine("Display Actual Balance -> [D]");
+            Console.WriteLine("Close App              -> [C]");
+            Console.WriteLine("------------------------------\n");
+        }
+
+        void MenuInputHandler()
+        {
+            string actionSelector;
+
+            Console.WriteLine("Choose Action : ");
+
+            actionSelector = Console.ReadLine();
+
+            switch (actionSelector)
+            {
+                case "T":
+                    break;
+                case "W":
+                    break;
+                case "C":
+                    Environment.Exit(1);
+                    break;
+                default:
+                    MenuInputHandler();
+                    break;
+            }
+        }
+    }
+}
---
+++ src/TMS.DotNet06.ATM/BankAccount.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TMS.DotNet06.ATM
+{
+    class BankAccount
+    {
+        public decimal AccountAmount { get; private set; }
+        public BankAccount(decimal accountAmount)
+        {
+            AccountAmount = accountAmount;
+        }
+    }
+}
