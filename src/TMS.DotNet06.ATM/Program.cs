890c6d45251cb1682b0d4dd9b6b6e2aeedc019d8
Kirill Banakh <66416263+InsanireDevices@users.noreply.github.com>
2021-01-19 10:10:21 +0300

    init: Add project

---
+++ src/TMS.DotNet06.ATM/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace TMS.DotNet06.ATM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Cash'O Mate!");
+            Console.ReadKey();
+        }
+    }
+}
