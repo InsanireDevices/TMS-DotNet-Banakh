918e7f9a4e51731f1272d3d20e2820287ced6382
Kirill Banakh <66416263+InsanireDevices@users.noreply.github.com>
2021-01-20 12:18:44 +0300

    feat: Add User model. Add exercise models.

---
+++ src/TMS.DotNet06.FitnessTracker/Core/Enums/Sex.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TMS.DotNet06.FitnessTracker.Core.Enums
+{
+    public enum Sex
+    {
+        Male = 0,
+        Female = 1,
+        Other = 2,
+    }
+}
---
+++ src/TMS.DotNet06.FitnessTracker/Core/Models/User.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TMS.DotNet06.FitnessTracker.Core.Enums;
+
+namespace TMS.DotNet06.FitnessTracker.Core.Models
+{
+    class User
+    {
+        public User(string name, double weight, double height, int age, Sex sex)
+        {
+            Name = name;
+            this.Weight = weight;
+            this.Height = height;
+            this.Age = age;
+            this.Sex = sex;
+        }
+        public string Name { get; set; }
+        public double Weight { get; set; }
+        public double Height { get; set; }
+        public int Age { get; set; }
+        public  Sex Sex { get; set; }
+
+        public List<Run> Runs { get; set; } = new List<Run>();
+        public List<PushUps> PushUps { get; set; } = new List<PushUps>();
+        public List<Squats> Squats { get; set; } = new List<Squats>();
+    }
+}
