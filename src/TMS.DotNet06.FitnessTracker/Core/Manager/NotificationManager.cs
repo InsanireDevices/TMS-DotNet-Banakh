d98e2b7130c6c5f26810535f598f9b6b41a8aba5
Kirill Banakh <66416263+InsanireDevices@users.noreply.github.com>
2021-01-21 14:23:56 +0300

    doc: Notifications documented

--- src/TMS.DotNet06.FitnessTracker/Core/Manager/NotificationManager.cs
+++ src/TMS.DotNet06.FitnessTracker/Core/Manager/NotificationManager.cs
@@ -6,13 +6,20 @@ namespace TMS.DotNet06.FitnessTracker.Core.Manager
 {
     class NotificationManager
     {
+        /// <summary>
+        /// Method to send status notification to console
+        /// </summary>
+        /// <param name="notification"></param>
         public static void SendStatusNotification(string notification)
         {
             Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine($"[{DateTime.Now}] : {notification}");
             Console.ResetColor();
         }
-
+        /// <summary>
+        /// Method to send error notification to console
+        /// </summary>
+        /// <param name="notification"></param>
         public static void SendErrorNotification(string notification)
         {
             Console.ForegroundColor = ConsoleColor.Red;
