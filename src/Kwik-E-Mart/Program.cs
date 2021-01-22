684db76f22ab703cc4ff04373e8efc241ef7b6f8
Kirill Banakh <66416263+InsanireDevices@users.noreply.github.com>
2021-01-22 09:56:37 +0300

    init: Add Kwik-E-Mart project

---
+++ src/Kwik-E-Mart/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace KwikEMart
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
